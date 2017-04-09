a2cfc110a855fd496609ffe9d44bac8d5321b9dc
Luiz Parreira <luizfilipester@gmail.com>
2017-04-09 00:01:33 -0300

    [wip] Simplified generator and made first ugly version able to create a file
    Now I need to:
    
    - Refactor the code
    - Understand how to make this work like a proper cli tool
    - Understand the way the loading works in this gem
    - imeplemt the creation of the files

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,14 +1,52 @@
+#!/usr/bin/env ruby
+
 require 'thor'
+# Public: Module responsible for generating use case files
+class Generator < Thor
+  include Thor::Actions
 
-module Caze
-  # Public: Module responsible for generating use case files 
-  module Generator
-    autoload :CLI, 'caze/generator/cli'
+  desc 'generate file',
+       'generates file and creates the class with its attributes'
+  # Define arguments and options
+  class_option :test_framework, default: :rspec
 
-    class << self
-      def caze
-        Caze::CazeGenerator::CLI.start(ARGV)
-      end
+  def generate(namespace)
+    file_path = Parser.namespace_to_path(namespace)
+
+    create_file file_path do
+      '#TODO: Implement logic to generate files'
     end
   end
 end
+
+class Parser
+  class << self
+    def namespace_to_path(namespace)
+      namespaces = namespace.split(':')
+      path_type = namespaces.first.to_sym
+      namespaces.delete_at 0
+      unless known_paths.include? path_type
+        path_type = :normal
+      end
+      path = ''
+      case path_type
+      when :engine, :gem
+        dependency_name = namespaces.first
+        namespaces.delete_at 0
+        "#{path_type}/#{dependency_name}/lib/#{dependency_name}/#{namespaces.join('/')}.rb"
+      when :modules
+        "app/#{path_type}/#{namespaces.join('/')}.rb"
+      else
+        "lib/#{namespaces.join('/')}.rb"
+      end
+    end
+
+    private
+
+    def known_paths
+      [:engine, :gem, :modules]
+    end
+  end
+end
+
+Generator.start
