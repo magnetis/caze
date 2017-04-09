5a3c0ac8d5cc4be3e647a72b6565e06e3c1f855d
Luiz Parreira <luizfilipester@gmail.com>
2017-04-09 15:48:07 -0300

    Added `known_path?` method to the module

--- lib/caze/generator.rb
+++ lib/caze/generator.rb
@@ -1,13 +1,24 @@
 # frozen_string_literal: true
 
 module Caze
+  # Public: Module responsible for generating use case files
   module Generator
-    # Public: Module responsible for generating use case files
     autoload :CLI, 'caze/generator/cli'
 
     class << self
       def start_generator
         CLI.start
+      end
+
+      def known_path?(path_type)
+        [
+          :engine,
+          :engines,
+          :gem,
+          :gems,
+          :modules,
+          :module
+        ].include? path_type
       end
     end
   end
